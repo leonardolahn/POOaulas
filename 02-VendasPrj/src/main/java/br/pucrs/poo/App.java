a08e47e1e8729cd8d8bdfd0e058eedb0de6d4331
Leonardo Lahn Fernandes <leonardolahnfernandes09@gmail.com>
2026-08-10 14:14:10 +0000

    Novo exercício VENDAS

---
+++ 02-VendasPrj/src/main/java/br/pucrs/poo/App.java
@@ -0,0 +1,10 @@
+package br.pucrs.poo;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(
+            "Hello, world! Projeto 02-VendasPrj criado com sucesso."
+        );
+    }
+}
